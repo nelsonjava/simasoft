a6e758930e62b93e37383e42b035f2dd3ea59458
nelsonfer1 <nelsonjava@gmail.com>
2015-03-07 15:36:15 -0500

    first commit

---
+++ contabWeb/src/main/java/co/simasoft/models/iso/lmr/Indexaciones.java
@@ -0,0 +1,56 @@
+package co.simasoft.models.iso.lmr;
+
+import co.simasoft.models.iso.lmr.*;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+public class Indexaciones {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.TABLE)
+    private long id;
+
+    private Integer optlock;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "indexaciones")
+    private Set<Lmrs> lmrs = new HashSet<Lmrs>();
+
+    public Indexaciones() {
+    }
+
+    public Indexaciones(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Lmrs> getLmrs() {
+        return lmrs;
+    }
+    public void setLmrs(Set<Lmrs> lmrs) {
+        this.lmrs = lmrs;
+    }
+
+} // entity
+
---
+++ contabWeb/src/main/java/co/simasoft/models/iso/procesos/Procesos.java
@@ -0,0 +1,96 @@
+package co.simasoft.models.iso.procesos;
+
+import co.simasoft.models.iso.procesos.*;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+public class Procesos {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.TABLE)
+    private long id;
+
+    private Integer optlock;
+
+    private String codigo;
+
+    private String nombre;
+
+    private String observaciones;
+
+    @OneToMany(mappedBy = "procesos")
+    private Set<Lmds> lmds = new HashSet<Lmds>();
+
+    @OneToMany(mappedBy = "procesos")
+    private Set<Lmrs> lmrs = new HashSet<Lmrs>();
+
+    @ManyToOne
+    private TiposProcesos tiposprocesos;
+
+    public Procesos() {
+    }
+
+    public Procesos(String codigo,String nombre,String observaciones) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.observaciones = observaciones;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public Set<Lmds> getLmds() {
+        return lmds;
+    }
+    public void setLmds(Set<Lmds> lmds) {
+        this.lmds = lmds;
+    }
+
+    public Set<Lmrs> getLmrs() {
+        return lmrs;
+    }
+    public void setLmrs(Set<Lmrs> lmrs) {
+        this.lmrs = lmrs;
+    }
+
+    public TiposProcesos getTiposProcesos() {
+        return tiposProcesos;
+    }
+    public void setTiposProcesos(TiposProcesos tiposProcesos) {
+        this.tiposProcesos = tiposProcesos;
+    }
+
+} // entity
+
