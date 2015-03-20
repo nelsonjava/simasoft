7465c928bfb2802ae257cc30b320e039ba593af8
nelsonfer1 <nelsonjava@gmail.com>
2015-03-19 22:49:47 -0500

    first commit

--- generator/src/main/java/co/simasoft/generator/App.java
+++ generator/src/main/java/co/simasoft/generator/App.java
@@ -1,5 +1,6 @@
 package co.simasoft.generator;
 
+import co.simasoft.utils.*;
 import co.simasoft.generator.utils.*;
 
 import java.io.*;
@@ -8,6 +9,10 @@ public class App{
 
     public static void main( String[] args ) throws IOException {
 
+        System.out.println("host name..........:" + Utils.hostName());
+        System.out.println("Current IP address.:" + Utils.hostIp());
+	System.out.println("Current MAC address:" + Utils.hostMac());
+	
         Ui.typeApp();
 
     } // main
