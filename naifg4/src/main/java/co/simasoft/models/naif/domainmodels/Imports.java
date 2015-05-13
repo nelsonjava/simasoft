8199a78a259d198f371c9b19d439d0c9f7f936b0
nelsonfer1 <nelsonjava@gmail.com>
2015-05-13 13:15:32 -0500

    first commit

---
+++ naifg4/src/main/java/co/simasoft/models/naif/domainmodels/Imports.java
@@ -0,0 +1,127 @@
+package co.simasoft.models.naif.domainmodels;
+
+import co.simasoft.models.naif.domainmodels.*;
+
+import java.io.*;
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+public class Imports implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private Long id;
+
+	@Version
+	private Integer optlock;
+
+	private long orden;
+
+	private String name;
+
+	private String observaciones;
+
+	@OneToMany(mappedBy = "imports")
+	private Set<PropertiesAttributes> propertiesAttributes = new HashSet<PropertiesAttributes>();
+
+	@ManyToOne
+	private Dependency dependency;
+
+	public Imports() {
+	}
+
+	public Imports(String name, String observaciones) {
+		this.name = name;
+		this.observaciones = observaciones;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getOptlock() {
+		return this.optlock;
+	}
+	public void setOptlock(Integer optlock) {
+		this.optlock = optlock;
+	}
+
+	public long getOrden() {
+		return this.orden;
+	}
+	public void setOrden(long orden) {
+		this.orden = orden;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public Set<PropertiesAttributes> getPropertiesAttributes() {
+		return propertiesAttributes;
+	}
+	public void setPropertiesAttributes(
+			Set<PropertiesAttributes> propertiesAttributes) {
+		this.propertiesAttributes = propertiesAttributes;
+	}
+
+	public Dependency getDependency() {
+		return dependency;
+	}
+	public void setDependency(Dependency dependency) {
+		this.dependency = dependency;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object ojt) {
+		if (this == ojt)
+			return true;
+		if (ojt == null)
+			return false;
+		if (getClass() != ojt.getClass())
+			return false;
+
+		Imports other = (Imports) ojt;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+} // entity
+
